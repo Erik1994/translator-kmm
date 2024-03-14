5b656b524000d3d1110634bb32dfe790933a9802
Erik Antonyan <erik.antonyan1994@gmail.com>
2024-03-14 17:59:03 +0400

    Implement VoiceToTextParser for Android

---
+++ androidApp/src/main/java/com/example/kmptranslator/android/voicetotext/di/VoiceToTextModule.kt
@@ -0,0 +1,22 @@
+package com.example.kmptranslator.android.voicetotext.di
+
+import android.app.Application
+import com.example.kmptranslator.android.voicetotext.data.AndroidVoiceToTextParser
+import com.example.kmptranslator.voicetotext.VoiceToTextParser
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
+
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object VoiceToTextModule {
+
+    @Provides
+    @ViewModelScoped
+    fun provideVoiceToTextParser(app: Application): VoiceToTextParser {
+        return AndroidVoiceToTextParser(app)
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/example/kmptranslator/voicetotext/VoiceToTextParser.kt
@@ -0,0 +1,11 @@
+package com.example.kmptranslator.voicetotext
+
+import com.example.kmptranslator.core.domain.util.CommonStateFlow
+
+interface VoiceToTextParser {
+    val state: CommonStateFlow<VoiceToTextParserState>
+    fun startListening(languageCode: String)
+    fun stopListening()
+    fun cancel()
+    fun reset()
+}
